762a388f4b0082de68d50555dbde2af348f92c72
uehara <b1015062@fun.ac.jp>
2018-04-21 14:43:39 +0900

    remove dev codes

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 import * as firebase from 'firebase';
 
@@ -7,7 +7,6 @@ import * as firebase from 'firebase';
 import firebaseConfig from "./firebaseConfig";
 
 firebase.initializeApp(firebaseConfig);
-let hoge = 'hoge';
 
 function storeHighScore() {
   firebase.database().ref('yamaokaya-dev/' + 'testList').push({
@@ -16,38 +15,17 @@ function storeHighScore() {
 
 }
 
-setupHighscoreListener(userId) {
-  firebase.database().ref('users/' + userId).on('value', (snapshot) => {
-    const highscore = snapshot.val().highscore;
-    console.log("New high score: " + highscore);
-  });
-}
-
-class FlatListBasics extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={firebase.database().ref('yamaokaya-dev/' + 'testList').limitToLast(2)}
-          renderItem={({item}) => <Text style={styles.container}>{toString(item)}</Text>}
-        />
-      </View>
-    );
-  }
-}
-
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>OISHII RAMEN FOUND!{hoge}</Text>
+        <Text>OISHII RAMEN FOUND!</Text>
         <Button
           onPress={storeHighScore}
-          title="Learn More"
+          title="募集をかける"
           color="#841584"
-          accessibilityLabel="Learn more about this purple button"
+          accessibilityLabel="募集する"
         />
-        <FlatListBasics />
       </View>
     );
   }
