79509fa806cc2936f88f1090babb7cb521a88b5d
uehara <b1015062@fun.ac.jp>
2018-04-21 23:49:25 +0900

    ListItem を拡張

--- ListItem.js
+++ ListItem.js
@@ -7,6 +7,8 @@ class ListItem extends Component {
   render() {
     return (
       <View>
+        <Text>店舗名　函館鍛冶店</Text>
+        <Text>日時 2018/04/28 18:00</Text>
         <Text>{this.props.item.comment}</Text>
       </View>
     );
