3c2a23163525f6925e92eed12715421ebff44312
ShiranuiNui <shiranuisayaka@gmail.com>
2018-04-22 13:56:30 +0900

    [Add] 募集追加画面の入力欄を追加

--- ui/AddModal.js
+++ ui/AddModal.js
@@ -1,10 +1,14 @@
-import React, {Component} from 'react';
-import {Modal, Text, Button, View} from 'react-native';
+import React, { Component } from 'react';
+import { Animated, Modal, Text, TextInput, Picker, Button, View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 
 class AddModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { comment: '', shop: '', time: '', comment: '' };
+  }
   render() {
     return (
-      <View style={{marginTop: 22}}>
+      <View style={{ marginTop: 22 }}>
         <Modal
           animationType="slide"
           transparent={false}
@@ -12,19 +16,34 @@ class AddModal extends Component {
           onRequestClose={() => {
             alert('Modal has been closed.');
           }}>
-          <View style={{marginTop: 22}}>
-              <Text>募集するよ</Text>
+          <View style={{ marginTop: 22 }}>
+            <Text>募集するよ</Text>
 
-              <Text>色々な項目</Text>
-              <Text>色々な項目</Text>
-              <Text>色々な項目</Text>
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+              onChangeText={(text) => this.setState({ shop })}
+              value={this.state.shop}
+              placeholder='店舗名を入力して下さい'
+            />
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+              onChangeText={(text) => this.setState({ time })}
+              value={this.state.time}
+              placeholder='時間を入力して下さい'
+            />
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+              onChangeText={(text) => this.setState({ comment })}
+              value={this.state.comment}
+              placeholder='コメントを入力して下さい（任意）'
+            />
 
-              <Button
-                title="募集をかける"
-                onPress={() => {
-                  this.props.onPress()
-                }}>
-              </Button>
+            <Button
+              title="募集をかける"
+              onPress={() => {
+                this.props.onPress()
+              }}>
+            </Button>
 
           </View>
         </Modal>
@@ -32,5 +51,9 @@ class AddModal extends Component {
     );
   }
 }
-
 module.exports = AddModal;
+/*
+<Picker.Item label="上磯店" value="KAMIISO" />
+<Picker.Item label="函館鍛冶店" value="KAJI" />
+<Picker.Item label="函館万代店" value="MANDAI" />
+*/
